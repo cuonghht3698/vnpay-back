ce3370b45af110ce3e42289346e5e6de8d94fec1
CuongNB <cuongnb3698@gmail.com>
2023-04-11 16:28:38 +0700

    call api

--- app.js
+++ app.js
@@ -20,3 +20,31 @@ if (isThanhCong == '00' || isThanhCong2 == '00') {
     document.getElementById('status').style.color = "red";
 }
 console.log(maHoaDon);
+
+const url = 'http://vtech.booksapi/DSDonHang/vnpay_response/';
+
+// Query params cần truy vấn
+const queryParams = { vnp_TxnRef: maHoaDon, vnp_Amount: (tien / 100), vnp_ResponseCode: isThanhCong };
+
+// Tạo URL với Query Params
+const urlWithQueryParams = new URL(url);
+Object.keys(queryParams).forEach(key => urlWithQueryParams.searchParams.append(key, queryParams[key]));
+
+// call api
+fetch(urlWithQueryParams, { method: 'GET' })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Failed to retrieve data because ' + response.statusText);
+        }
+        return response.json();
+    })
+    .then(data => {
+        console.log(data);
+        // Xử lý dữ liệu được trả về từ server
+    })
+    .catch(error => {
+        console.error('Error:', error);
+        // Xử lý lỗi
+    });
+
+
